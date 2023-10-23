27b8f5a5f63a7e8fb485ed14f03cf0cfbd822a0d
Carlos Loureda <carloslouredaparrado@gmail.com>
2023-10-23 13:40:34 +0200

    chore: update outdated packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { worker } from "@uidotdev/react-query-api";
+import "./index.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 new Promise((res) => setTimeout(res, 100))
   .then(() =>
@@ -13,14 +16,13 @@ new Promise((res) => setTimeout(res, 100))
     })
   )
   .then(() => {
-    ReactDOM.render(
+    root.render(
       <React.StrictMode>
         <BrowserRouter>
           <div className="container">
             <App />
           </div>
         </BrowserRouter>
-      </React.StrictMode>,
-      document.getElementById("root")
+      </React.StrictMode>
     );
   });
