eacbe1f0d1f6e6ec0e79fdc2dbbfc904e85208e8
Carlos Loureda <carloslouredaparrado@gmail.com>
2023-10-31 04:28:09 +0100

    feat: add issue form/page

--- src/pages/AddIssue.tsx
+++ src/pages/AddIssue.tsx
@@ -1,5 +1,50 @@
 import React from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export function AddIssue() {
-  return <h2>Add Issue</h2>;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const addIssue = useMutation({
+    mutationFn: ({ comment, title }: { comment: string; title: string }) =>
+      fetch("/api/issues", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ comment, title }),
+      }).then((res) => res.json()),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["issues"], exact: true });
+      queryClient.setQueryData(["issues", data.number.toString()], data);
+      navigate(`/issue/${data.number}`);
+    },
+  });
+  return (
+    <div className="add-issue">
+      <h2>Add Issue</h2>
+      <form
+        onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          if (addIssue.isPending) return;
+
+          const target = event.target as typeof event.target & {
+            comment: { value: string };
+            title: { value: string };
+          };
+
+          addIssue.mutate({
+            comment: target.comment.value,
+            title: target.title.value,
+          });
+        }}
+      >
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" placeholder="Title" name="title" />
+        <label htmlFor="comment">Comment</label>
+        <textarea placeholder="Comment" id="comment" name="comment" />
+        <button type="submit" disabled={addIssue.isPending}>
+          {addIssue.isPending ? "Adding Issue..." : "Add Issue"}
+        </button>
+      </form>
+    </div>
+  );
 }
--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IssuesList } from "../components/IssuesList";
 import { LabelList } from "../components/LabelList";
 import { StatusSelect } from "../components/StatusSelect";
+import { Link } from "react-router-dom";
 
 export function Issues() {
   const [labels, setLabels] = React.useState<string[]>([]);
@@ -34,6 +35,10 @@ export function Issues() {
               setStatus(event.target.value)
             }
           />
+          <hr />
+          <Link className="button" to="/add">
+            Add Issue
+          </Link>
         </aside>
       </main>
     </div>
